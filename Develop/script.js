a9d999664e0877802fe87e1d607eaf7c16e63516
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-07-06 19:44:42 -0400

    added a function to store user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,23 @@
+// Current Day Display using MOMENT
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
-$("currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
+// Store user input 
+
+function toDoEvent() { 
+var userInput = $(this).siblings("input").val().trim();
+var hourId = $(this).siblings("input").attr("id");
+
+localStorage.setItem(hourId, userInput);
+
+console.log("user input")
+
+}
+
+$(".saveBTN").on("click", toDoEvent)
+
+
 
