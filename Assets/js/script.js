bcc433fb3bfd2a751926f02c359797c82f07a369
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-07-19 14:01:24 -0400

    added the clear schedule button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,39 @@
+// Time and Date using Moment.js 
+
 var now = moment().format('MMMM Do YYYY, h:mm a');
 $("#currentDay").text(now);
 
 
-
 // Store user input 
 
-function toDoEvent(event) { 
-var userInput = $(this).siblings("input").val().trim();
-var hourId = $(this).siblings("input").attr("id");
+function toDoEvent(event) {
+    var userInput = $(this).siblings("input").val().trim();
+    var hourId = $(this).siblings("input").attr("id");
 
-localStorage.setItem(hourId, userInput);
+    localStorage.setItem(hourId, userInput);
 
-console.log("user input")
+    console.log("user input")
 
 }
 
 $(".saveBtn").on("click", toDoEvent)
+
+
+
+
+
+
+
+
+
+
+// Clear Button function 
+
+$("#clear").click(function () {
+    localStorage.clear();
+    location.reload();
+    console.log("clicked")
+});
 
 
 
